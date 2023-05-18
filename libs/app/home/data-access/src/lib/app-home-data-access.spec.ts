b7401070466461dc5698d431dac3a64a7108b1f1
Emilee da Cruz <u21494577@tuks.co.za>
2023-05-18 10:25:09 +0200

    creating branch.

---
+++ libs/app/home/data-access/src/lib/app-home-data-access.spec.ts
@@ -0,0 +1,7 @@
+import { appHomeDataAccess } from './app-home-data-access';
+
+describe('appHomeDataAccess', () => {
+  it('should work', () => {
+    expect(appHomeDataAccess()).toEqual('app-home-data-access');
+  });
+});
