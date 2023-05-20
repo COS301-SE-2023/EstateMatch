b96240ecd5b638d3ec471619908d69e2070ca535
FuryOfD <dharshanng@gmail.com>
2023-05-20 23:09:12 +0200

    🔥 (api) removed propertise lib folder

--- libs/api/properties/schema/src/lib/api-properties-schema.spec.ts
+++
@@ -1,7 +0,0 @@
-import { apiPropertiesSchema } from './api-properties-schema';
-
-describe('apiPropertiesSchema', () => {
-  it('should work', () => {
-    expect(apiPropertiesSchema()).toEqual('api-properties-schema');
-  });
-});
--- libs/api/properties/schema/src/lib/api-properties-schema.ts
+++
@@ -1,3 +0,0 @@
-export function apiPropertiesSchema(): string {
-  return 'api-properties-schema';
-}
