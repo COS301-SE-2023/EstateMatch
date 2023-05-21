0b1b9f9ac78cba99bddb3073169b72db3e4a9fd0
Emilee da Cruz <u21494577@tuks.co.za>
2023-05-21 19:11:15 +0200

    💄changed components

---
+++ libs/app/home/feature/src/CardChange.js
@@ -0,0 +1,8 @@
+document.getElementById('like-button').addEventListener('click', function() {
+    alert('Like button clicked');
+  });
+  
+  document.getElementById('dislike-button').addEventListener('click', function() {
+    alert('Dislike button clicked');
+  });
+  
