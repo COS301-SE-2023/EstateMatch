2ff871e0f3b237f8d95b7e0dcb6ed88183e77976
DvZ <danielvzyl1469@gmail.com>
2023-05-21 22:27:14 +0200

    ✨ (app) Integrated with api

--- libs/app/preferences/feature/src/preferences.module.ts
+++ libs/app/preferences/feature/src/preferences.module.ts
@@ -4,9 +4,17 @@ import { IonicModule } from '@ionic/angular';
 // import { ProfileModule } from '@mp/app/profile/data-access';
 import { PreferencesPage } from './preferences.page';
 import { PreferencesRouting } from './preferences.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, PreferencesRouting],
+  imports: [
+    CommonModule,
+    IonicModule,
+    PreferencesRouting, 
+    FormsModule, 
+    HttpClientModule
+  ],
   declarations: [PreferencesPage],
 })
 
