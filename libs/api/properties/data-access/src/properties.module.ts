6f22434a7e86f6ccf357a186dd86e4727ddcf85d
DvZ <danielvzyl1469@gmail.com>
2023-05-22 00:03:10 +0200

    🐛 (api) fixed properties da module

--- libs/api/properties/data-access/src/properties.module.ts
+++ libs/api/properties/data-access/src/properties.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LikedPropertiesRepository } from './liked-property.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LikedPropertiesSchema } from '@estate-match/api/properties/schema';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{name : 'LikedProperties', schema : LikedPropertiesSchema}]),
+],
   providers: [LikedPropertiesRepository],
   exports: [LikedPropertiesRepository],
 })
