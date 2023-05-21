ad8bfcbb3a9dae0cad7ce8bf5798eba0f5dbd576
DvZ <danielvzyl1469@gmail.com>
2023-05-21 23:30:33 +0200

    🚧 (app) Integration with api

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -1,15 +1,65 @@
 import { Component } from '@angular/core';
 
+interface Property {
+  user: string;
+  address: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  amenities: string[];
+  liked: boolean;
+}
+
 @Component({
   selector: 'ms-home-page',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
+
 export class HomePage {
-  descriptions: string[] = ['Three Bedroom and Two Bathrooms.',
-   'Four Bedroom and 3 Bathrooms. With an indoor pool.',
-   '2 Bedroom house in a good neighbourhood. 24hr Security.',
-    '3 Bedroom and One bathroom, with a pool.'];
+
+  properties: Property[] = [
+    {
+      user: 'Jack Daniels',
+      address: 'Ballito, KZN',
+      price: 5000000,
+      bedrooms: 3,
+      bathrooms: 2,
+      amenities: [],
+      liked: false,
+    },
+    {
+      user: 'Jack Daniels',
+      address: 'Salt Rock, KZN',
+      price: 10000000,
+      bedrooms: 4,
+      bathrooms: 3,
+      amenities: ['Indoor Pool'],
+      liked: false,
+    },
+    {
+      user: 'Jack Daniels',
+      address: 'Kyalami, Gauteng',
+      price: 3000000,
+      bedrooms: 2,
+      bathrooms: 1,
+      amenities: ['24hr Security'],
+      liked: false,
+    },
+    {
+      user: 'Jack Daniels',
+      address: 'Vereeniging, Gauteng',
+      price: 15000000,
+      bedrooms: 3,
+      bathrooms: 1,
+      amenities: ['Pool'],
+      liked: false,
+    },
+  ];
+  descriptions: string[] = ['R5 000 000. Three Bedroom and Two Bathrooms.',
+   'R10 000 000. Four Bedroom and 3 Bathrooms. With an indoor pool.',
+   'R3 000 000. 2 Bedroom house in a good neighbourhood. 24hr Security.',
+    'R1 500 000.3 Bedroom and One bathroom, with a pool.'];
   images: string[] = [
     'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg',
     'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -18,9 +68,20 @@ export class HomePage {
   ];
   area: string[] = ['Ballito, KZN', 'Salt Rock, KZN', 'Kyalami, Gauteng', 'Vereeniging, Gauteng']; 
 
-  currentDescriptionIndex: number = 0;
+  currentDescriptionIndex = 0;
 
-  likeHouse() {
+  likeHouse() { 
+    // if(this.currentDescriptionIndex == 0){
+    //   console.log(this.properties[0]);
+    // }else{
+    //   console.log(this.properties[this.currentDescriptionIndex - 1]);
+    // }
+
+    const currProperty = this.properties[this.currentDescriptionIndex];
+    currProperty.liked = true;
+    console.log(this.properties[this.currentDescriptionIndex]);
+
+
     this.currentDescriptionIndex++;
     if (this.currentDescriptionIndex >= this.descriptions.length) {
       this.currentDescriptionIndex = 0;
@@ -28,6 +89,9 @@ export class HomePage {
   }
 
   dislikeHouse() {
+    const currProperty = this.properties[this.currentDescriptionIndex];
+    currProperty.liked = false;
+    console.log(this.properties[this.currentDescriptionIndex]);
     this.currentDescriptionIndex++;
     if (this.currentDescriptionIndex >= this.descriptions.length) {
       this.currentDescriptionIndex = 0;
