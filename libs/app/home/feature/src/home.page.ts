5b72da93fe0203f286dc5744661d18d0bde4726c
Emilee da Cruz <u21494577@tuks.co.za>
2023-05-21 22:10:07 +0200

    ✨ like/dislike changing card content

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -6,11 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  LikeHouse(){
-    alert('like button clicked');
+  descriptions: string[] = ['Description 1', 'Description 2', 'Description 3', 'Description 4'];
+  /*images: string[] = ['https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg0',
+                      'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+                      'https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg', 
+                      'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg']; */
+  area: string[] = ['Ballito, KZN', 'Salt Rock, KZN', 'Kyalami, Gauteng', 'Vereeniging, Gauteng']; 
+
+  currentDescriptionIndex: number = 0;
+
+  likeHouse() {
+    this.currentDescriptionIndex++;
+    if (this.currentDescriptionIndex >= this.descriptions.length) {
+      this.currentDescriptionIndex = 0;
+    }
   }
-  DislikeHouse(){
-    alert('Dislike button clicked');
+
+  dislikeHouse() {
+    this.currentDescriptionIndex++;
+    if (this.currentDescriptionIndex >= this.descriptions.length) {
+      this.currentDescriptionIndex = 0;
+    }
   }
 }
+
+
+
+
+
+
 
