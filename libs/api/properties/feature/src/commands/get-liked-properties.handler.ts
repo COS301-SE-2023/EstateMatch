9dadb0810b109a88bdbfd894a8b1b50ce231552d
Adam <u21529583@tuks.co.za>
2023-05-22 16:55:27 +0200

    🎨 Added get-liked-properties

---
+++ libs/api/properties/feature/src/commands/get-liked-properties.handler.ts
@@ -0,0 +1,18 @@
+import { LikedPropertiesRepository } from "@estate-match/api/properties/data-access";
+import { GetLikedPropertiesCommand, IGetLikedPropertiesResponse } from "@estate-match/api/properties/util";
+import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
+
+@CommandHandler(GetLikedPropertiesCommand)
+export class GetLikedPropertiesHandler implements ICommandHandler<GetLikedPropertiesCommand> {
+    constructor(
+        private readonly repository: LikedPropertiesRepository,
+        private readonly publisher: EventPublisher
+    ) {}
+
+    async execute(command: GetLikedPropertiesCommand): Promise<any> {
+       const request = command.request;
+       const user = request.user;
+
+       //return this.repository.findOne({ user });
+    }
+}
