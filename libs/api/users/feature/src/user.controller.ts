0f69c4fd76763610aa8c01b81cc95675aa1b0d30
FuryOfD <dharshanng@gmail.com>
2023-05-31 11:49:40 +0200

    🚧 (api) user controller created

---
+++ libs/api/users/feature/src/user.controller.ts
@@ -0,0 +1,11 @@
+import {Body, Controller, Get, Post} from '@nestjs/common';
+//import {UserServices} from './user.service';
+//import {IGetUserRequest , ISetUserRequest } from '@estate-match/api/users/util';
+
+// @Controller()
+// export class UserController {
+//     constructor(/*private readonly userServices: UserServices*/) {}
+
+
+
+// } 
