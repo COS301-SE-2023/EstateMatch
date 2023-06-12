8fb16c0275a69f69f862e1e04dff1f8ddda08ba2
DvZ <danielvzyl1469@gmail.com>
2023-06-12 20:08:14 +0200

    ✨ (app) Login integrated

--- libs/app/login/feature/src/login.page.ts
+++ libs/app/login/feature/src/login.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ms-login-page',
@@ -7,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  constructor(private http: HttpClient,
+    private toastController: ToastController,
+    private router: Router) { }
+  username = '';
+  password = '';
   
+  async login() {
+    const url = 'api/login';
+    const body = {
+        username: this.username,
+        password: this.password,
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    const loggedIn = await this.http.post(url, body, { headers }).toPromise() as {message: string};
+    if(loggedIn.message === 'User Login Success'){
+      this.makeToast('Login success');
+      this.router.navigate(['/home'], { replaceUrl: true });
+    }else{
+      this.makeToast('Login failed');
+    }
+  }
+
+  async makeToast(message: any){
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      position: 'top',
+    })
+    toast.present();
+  }
 }
