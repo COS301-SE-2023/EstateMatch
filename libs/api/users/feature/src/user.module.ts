1775510e10565d7d19a664642da04dae71f262af
FuryOfD <dharshanng@gmail.com>
2023-06-15 11:04:14 +0200

    🚧 (api) user module created

---
+++ libs/api/users/feature/src/user.module.ts
@@ -0,0 +1,25 @@
+import {Module} from '@nestjs/common';  
+import {UserController} from './user.controller';
+import {UserService} from './user.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import {UserModule as UserDataAccessModule} from '@estate-match/api/users/data-access';
+
+//import handlers
+import {GetUserHandler, SetUserHandler} from './commands';
+import exp = require('constants');
+
+export const UserCommandHandlers = [
+    GetUserHandler,
+    SetUserHandler
+];
+
+@Module({
+    imports: [UserDataAccessModule, CqrsModule],
+    controllers: [UserController],
+    providers: [
+        UserService,
+        ...UserCommandHandlers
+    ],
+    exports: [UserService]
+})
+export class UserModule {}
