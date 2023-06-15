77d5f40d3a91f5d0d1efa86b2aa790975add31b4
FuryOfD <dharshanng@gmail.com>
2023-06-15 21:31:17 +0200

    🚧 (api) get-property request

---
+++ libs/api/properties/util/src/requests/get-properties.request.ts
@@ -0,0 +1,12 @@
+export interface IProperty{
+    _id: string, //could say users here 
+    address: string,
+    price: number,
+    bedrooms: number,
+    bathrooms: number,
+    garages: number,
+    amenities: string[],
+    liked: boolean,
+    image: string,
+    
+}
--- libs/api/properties/util/src/requests/index.ts
+++ libs/api/properties/util/src/requests/index.ts
@@ -1,3 +1,4 @@
 export * from './dislike-property.request';
 export * from './like-property.request';
 export * from './get-liked-properties.request';
+export * from './get-properties.request';
