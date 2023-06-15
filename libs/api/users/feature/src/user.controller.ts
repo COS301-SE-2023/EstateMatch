a0ccb75a03e288c575e721512e6c393d52767e31
FuryOfD <dharshanng@gmail.com>
2023-06-15 10:49:40 +0200

    🚧 (api) user services created

--- libs/api/users/feature/src/user.controller.ts
+++ libs/api/users/feature/src/user.controller.ts
@@ -1,11 +1,2 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
-//import {UserServices} from './user.service';
-//import {IGetUserRequest , ISetUserRequest } from '@estate-match/api/users/util';
-
-// @Controller()
-// export class UserController {
-//     constructor(/*private readonly userServices: UserServices*/) {}
-
-
-
-// } 
+import {Controller, Post, Body, Get} from '@nestjs/common';
+//import {UserService} from './user.service';
---
+++ libs/api/users/feature/src/user.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import { GetUserCommand, IGetUserRequest, IGetUserResponse } from '@estate-match/api/users/util';
+import { SetUserCommand, ISetUserRequest, ISetUserResponse } from '@estate-match/api/users/util';
+import { CommandBus } from '@nestjs/cqrs';
+
+@Injectable()
+export class UserService {
+    constructor(private readonly commandBus: CommandBus) {}
+    
+    async getUser(request: IGetUserRequest): Promise<IGetUserResponse> {
+        return await this.commandBus.execute<
+            GetUserCommand, IGetUserResponse>(new GetUserCommand(request));
+    }
+    
+    async setUser(request: ISetUserRequest): Promise<ISetUserResponse> {
+       return await this.commandBus.execute<
+            SetUserCommand, ISetUserResponse>(new SetUserCommand(request));
+    }
+}
