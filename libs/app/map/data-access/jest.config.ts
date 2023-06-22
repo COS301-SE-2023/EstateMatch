a4eee044c087ec86c206ef29adf3f1cdb48f3312
Liam <prinsloo.liam2@gmail.com>
2023-06-22 10:00:55 +0200

    ✨ (feat/user)added map feature

---
+++ libs/app/map/data-access/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'app-map-data-access',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/app/map/data-access',
+};
---
+++ libs/app/map/data-access/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/app-map-data-access';
