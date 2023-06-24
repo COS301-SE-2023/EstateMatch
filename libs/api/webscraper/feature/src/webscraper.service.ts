6ea4aa281c5921273800c9784677b5879441d211
Adam <u21529583@tuks.co.za>
2023-06-24 23:29:19 +0200

    🚧 (api) PP Description

--- libs/api/webscraper/feature/src/webscraper.service.ts
+++ libs/api/webscraper/feature/src/webscraper.service.ts
@@ -31,25 +31,76 @@ export class WebScraperService {
       
       await propertyPage.waitForSelector('.contentWhite');
 
-      //const imageURLs: string[]  = await propertyPage.$$eval('.js_lightboxImageWrapper', (imagesElement) => imagesElement.map((image) => image.getAttribute('data-image-url') || ''));
       const title = await propertyPage.$eval('.titleContainer h1', (titleElement) => titleElement.textContent?.trim() || '');
       const price = await propertyPage.$eval('.titleContainer h2 span.detailsPrice', (priceElement) => priceElement.textContent?.trim() || '');
-      //const description = await propertyPage.$eval('.description p', (descriptionElement) => descriptionElement.textContent?.trim() || '');
-      //const bedrooms = await propertyPage.$eval('li.p24_feaureDetails[title = "Bedrooms"] span ', (bedroomElement) => bedroomElement.textContent?.trim() || '');
-      //const bathrooms = await propertyPage.$eval('.p24_feautureDetails[title = "Bathrooms"] span', (bathroomElement) => bathroomElement.textContent?.trim() || '');
-      //const garages = await propertyPage.$eval('.p24_feautureDetails[title = "Parking spaces"] span', (garageElement) => garageElement.textContent?.trim() || '');
+      const description = await propertyPage.$$eval('.description p', (descriptionElement) => descriptionElement.map((description) => description.textContent?.trim() || ''));
+      //const location = await propertyPage.$eval('.previousPage span', (locationElement) => locationElement.textContent?.trim() || '');
+      //const attributeLabel = await propertyPage.$$eval('.attributeLabel', (attributeLabelElement) => attributeLabelElement.map((attributeLabel) => attributeLabel.textContent?.trim() || ''));
+      //const propAttrValue = await propertyPage.$$eval('.propAttrValue', (propAttrValueElement) => propAttrValueElement.map((propAttrValue) => propAttrValue.textContent?.trim() || ''));
+    
+      //var bedrooms;
+      //var bathrooms;
+      //var garages;
+      //var amenities: string[] = [];
 
+      //console.log(attributeLabel);
+
+      // Get the text content of the <span> element
+      //const attributeLabelText = await propertyPage.evaluate(span => span?.textContent, attributeLabel);
+
+      // Check if the text content matches the desired value
+      /*for (const attribute of attributeLabel) 
+      {
+        if (attribute === 'Bedrooms') 
+        {
+          // Get the text content of the <div> element
+          bedrooms= propAttrValue[attributeLabel.indexOf(attribute)];
+          //console.log(bedrooms);
+        }
+
+        if (attribute === 'Bathrooms')
+        {
+          bathrooms= propAttrValue[attributeLabel.indexOf(attribute)];
+          //console.log(bathrooms);
+        }
+
+        if (attribute === 'Covered Parkings' || attribute === 'Open Parkings' || attribute === 'Garages')
+        {
+          garages= propAttrValue[attributeLabel.indexOf(attribute)];
+          //console.log(garages);
+        }
+
+        if(attribute !== 'Bedrooms' && attribute !== 'Bathrooms' && attribute !== 'Covered Parkings' && attribute !== 'Open Parkings' && attribute !== 'Garages' && attribute !== 'Lounges')
+        {
+          amenities.push(attribute);
+        }
+      }
+
+
+      const imageURLs = (await propertyPage.$$eval('.imageGrid a', (imagesElement) => imagesElement.map((image) => image.dataset['background']))).filter(url => url !== null && url !== undefined);
+
+      for (let i = 0; i < imageURLs.length; i++) {
+        const lastDotIndex = imageURLs[i]?.lastIndexOf(".");
+        if (lastDotIndex !== -1) 
+        {
+          imageURLs[i] = imageURLs[i]?.slice(0, lastDotIndex) + "_dhd" + imageURLs[i]?.slice(lastDotIndex);
+        }
+      }*/
+    
 
       await propertyPage.close();
 
       return {
-        //imageURLs,
         title,
         price,
-        //description,
+        description,
         //bedrooms,
         //bathrooms,
         //garages,
+        //imageURLs,
+        //location,
+        //amenities,
+
         
       };
     })
