513e6c12009d85b5d9c3258897d32ca2ee80bb36
Emilee da Cruz <u21494577@tuks.co.za>
2023-06-25 02:30:13 +0200

    ✨ keyframes for swiping

---
+++ libs/app/home/feature/src/keyframes.ts
@@ -0,0 +1,11 @@
+import { keyframes, style,animate } from '@angular/animations';
+
+export const swiperight = [
+  style({ opacity: 1 }),
+  style({ transform: 'translate3d(200%, 0, 0) rotate3d(0, 0, 1, 120deg)', opacity: 0 }),
+]
+
+export const swipeleft = [
+  style({ opacity: 1 }),
+  style({ transform: 'translate3d(-200%, 0, 0) rotate3d(0, 0, 1, -120deg)', opacity: 0 }),
+]
