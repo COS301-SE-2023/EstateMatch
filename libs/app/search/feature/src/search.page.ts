dd0f1d8f47d6911d4f63dd7fdf8b468021c64efa
DvZ <danielvzyl1469@gmail.com>
2023-06-26 13:09:12 +0200

    🚧 (app) Display search results

--- libs/app/search/feature/src/search.page.ts
+++ libs/app/search/feature/src/search.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'ms-search-page',
@@ -10,7 +12,12 @@ import { ToastController } from '@ionic/angular';
 
 export class SearchPage {
   constructor(private http: HttpClient,
-    private toastController: ToastController) {}
+    private toastController: ToastController,
+    private readonly router: Router) {}
 
     location = '';
+    
+    setFilters(data: string){
+      this.router.navigate(['/filter'], { queryParams: { data: data }, replaceUrl: true});
+    }
 }
