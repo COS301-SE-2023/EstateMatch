e87d4a4b48fe746f7ea37722846838a6668616bb
FuryOfD <dharshanng@gmail.com>
2023-07-12 23:31:22 +0200

    🚧 (test) cypress config files added

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    projectId : "agbje8",
-    baseUrl : 'http://localhost:3000',
-}
+  projectId: "agbje8",
+//   baseUrl: "http://localhost:3000",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
