d217346bc2aee4213095a8ae57738292203aeda6
FuryOfD <dharshanng@gmail.com>
2023-07-12 23:35:44 +0200

    🚧 (test) added first test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
   })
-})
+});
