932896dbc3b59604212fa3a73369e7c0b9628c15
FuryOfD <dharshanng@gmail.com>
2023-07-14 01:37:06 +0200

    🚧 (test) new test added fro Register

--- cypress/e2e/Register-spec.cy.js
+++ cypress/e2e/Register-spec.cy.js
@@ -4,5 +4,24 @@ describe('Register Page Test', () => {
         cy.visit('http://localhost:4200/register')
     })
 
+    it('enter all details needed to register', () => {
+        cy.get('ion-input[placeholder="Enter First Name"]').type('TestFirstName');
+        cy.get('ion-input[placeholder="Enter Last Name"]').type('TestLastName');
+        cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
+        cy.get('ion-input[placeholder="Enter Email Address"]').type('testemail@gmail.com');
+        cy.get('ion-input[placeholder="Enter Your Password"]').type('TestPassword');
+        cy.get('ion-input[placeholder="Confirm Password"]').type('TestPassword');
+
+    })
+
     
+    it('check if the password and confirmed password are the same', () => {
+        
+
+    })  
+
+    it('registers a new user', () => {
+
+    })
+
 });
