fe07ab6c63acc0d387c71aebf4b27839d9f4cd16
FuryOfD <dharshanng@gmail.com>
2023-07-14 01:09:45 +0200

    🚧 (test) added register page test

---
+++ cypress/e2e/Register-spec.cy.js
@@ -0,0 +1,8 @@
+describe('Register Page Test', () => {
+    beforeEach(() => {
+        cy.viewport(375, 812) //iPhone X size
+        cy.visit('http://localhost:4200/register')
+    })
+
+    
+});
