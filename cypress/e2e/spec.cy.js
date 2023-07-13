d60d66ba10e50522b6f61b57efd3acb3325860e2
FuryOfD <dharshanng@gmail.com>
2023-07-13 14:57:00 +0200

    🚧 (test) linking cypress to app

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+  it('clicks Signup on the login page', () => {
+    cy.visit('http://localhost:4200/login')
   })
 });
