5781d5ab5e939e85ae1408bc5b43472f152b7a07
FuryOfD <dharshanng@gmail.com>
2023-07-16 20:24:08 +0200

    ✅ (test) Home page test completed

--- cypress/e2e/Home-spec.cy.js
+++ cypress/e2e/Home-spec.cy.js
@@ -1,20 +1,24 @@
 describe('Home Page Test', () => {
-    it('User likes and dislikes a property', () => {
-        cy.viewport(375, 667) //iPhone X size
-        cy.visit('http://localhost:4200/login')
-        cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
-        cy.get('ion-input[placeholder="Password"]').type('TestPassword');
-        cy.contains('ion-button', ' Login ').click();
+  beforeEach(() => {
+    cy.viewport(375, 812); //iPhone X size
+    cy.visit('http://localhost:4200/login');
+    cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
+    cy.get('ion-input[placeholder="Password"]').type('TestPassword');
+    cy.contains('ion-button', ' Login ').click();
+    cy.wait(3000); //wait 3 seconds for the page to load
+  });
 
+  it('User likes and dislikes a property', () => {
+    // cy.viewport(375, 667) //iPhone X size
+    // cy.visit('http://localhost:4200/login')
+    // cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
+    // cy.get('ion-input[placeholder="Password"]').type('TestPassword');
+    // cy.contains('ion-button', ' Login ').click();
 
-        cy.wait(5000); //wait 5 seconds for the page to load
-        cy.get('button[id="like-button"]').click();
+    //cy.wait(3000); //wait 3 seconds for the page to load
+    cy.get('button[id="like-button"]').click();
 
-        cy.wait(5000); //wait 5 seconds for the page to load
-        cy.get('button[id="dislike-button"]').click();
-
-    });
-
-
-    
+    cy.wait(3000); //wait 3 seconds for the page to load
+    cy.get('button[id="dislike-button"]').click();
+  });
 });
