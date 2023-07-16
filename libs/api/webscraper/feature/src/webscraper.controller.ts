2a0e241d1ae1d0d79731e8b9b583c36cfa2b6072
Adam <u21529583@tuks.co.za>
2023-07-16 15:49:00 +0200

    🚧 (api) Added endpoint for Remax Rentals

--- libs/api/webscraper/feature/src/webscraper.controller.ts
+++ libs/api/webscraper/feature/src/webscraper.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from "@nestjs/common";
 import { PrivatePropertySaleService } from "./PrivatePropertySale.service";
 import { PrivatePropertyRentService } from "./PrivatePropertyRent.service";
 import { RemaxSaleService } from "./RemaxSale.service";
+import { RemaxRentService } from "./RemaxRent.service";
 import { PropertiesService } from "@estate-match/api/properties/feature";
 import { ICreatePropertyRequest, IProperty } from "@estate-match/api/properties/util";
 
@@ -10,6 +11,7 @@ export class WebScraperController {
     constructor(private readonly PrivatePropertySaleService: PrivatePropertySaleService,
       private readonly PrivatePropertyRentService: PrivatePropertyRentService,
       private readonly RemaxSaleService: RemaxSaleService,
+      private readonly RemaxRentService: RemaxRentService,
       private readonly propertyService: PropertiesService
       ) {}
     
@@ -87,6 +89,31 @@ export class WebScraperController {
       
       return properties;
     }
+
+    @Get("/RemaxRentScraper")
+    async getScrapedRemaxRentProperties() {
+        const properties = await this.RemaxRentService.RemaxRentscrape();
+      
+        for(let i = 0; i < properties.length; i++){
+          const property: IProperty = {
+            title: properties[i].title,
+            price: parseInt(properties[i].price.replace(/\s/g, "")),
+            bedrooms: properties[i].bedrooms,
+            bathrooms: properties[i].bathrooms,
+            garages: properties[i].garages,
+            location: properties[i].location,
+            amenities: properties[i].amenities,
+            images: properties[i].imageURLs,
+        }
+          const request: ICreatePropertyRequest = {
+            property: property
+          };
+          // console.log(request);
+          this.propertyService.createProperty(request);
+        }
+        
+        return properties;
+      }
 
 
 
