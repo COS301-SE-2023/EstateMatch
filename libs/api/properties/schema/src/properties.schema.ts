a5603a47fdb4a940827eee43e749b65161a79c15
FuryOfD <dharshanng@gmail.com>
2023-07-17 22:52:52 +0200

    🚧 (api) Property schema updated

--- libs/api/properties/schema/src/properties.schema.ts
+++ libs/api/properties/schema/src/properties.schema.ts
@@ -8,6 +8,10 @@ export const PropertiesSchema = new mongoose.Schema({
     bathrooms: Number,
     garages: Number,
     amenities: [String],
+
+    //what Adam wants
+    title : String,
+   // HouseType : Rent/Sale
 });
 
 export interface PropertiesModel {
@@ -18,4 +22,8 @@ export interface PropertiesModel {
     bathrooms: number,
     garages: number,
     amenities: string[],
+
+    //what Adam wants
+    title : string,
+    // HouseType : Rent/Sale
 }
