0fb983541a21d8a5993cccc98f3d69e10cdc463c
FuryOfD <dharshanng@gmail.com>
2023-07-18 16:14:53 +0200

    ✅ (test) updates on Login and Register

--- cypress/e2e/Login-spec.cy.js
+++ cypress/e2e/Login-spec.cy.js
@@ -1,30 +1,37 @@
-describe('Login and Register Page Test', () => {
+describe('Login Page Test', () => {
   beforeEach(() => {
     cy.viewport(375, 812); //iPhone X size
     cy.visit('http://localhost:4200/login');
     cy.wait(3000); //wait 3 seconds for the page to load
   });
 
-  it('user enters username and password and logins', () => {
+  it('user enters valid username and password', () => {
     cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
     cy.get('ion-input[placeholder="Password"]').type('TestPassword');
     cy.contains('ion-button', ' Login ').click();
     cy.wait(3000);
   });
 
-  it('clicks Signup on the Login page', () => {
-    cy.contains('ion-label', "Don't have an account ? Sign Up.").click();
-
-    cy.get('ion-input[placeholder="Enter First Name"]').type('TestFirstName');
-    cy.get('ion-input[placeholder="Enter Last Name"]').type('TestLastName');
-    cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
-    cy.get('ion-input[placeholder="Enter Email Address"]').type(
-      'testemail@gmail.com'
-    );
-    cy.get('ion-input[placeholder="Enter Your Password"]').type('TestPassword');
-    cy.get('ion-input[placeholder="Confirm Password"]').type('TestPassword');
-
-    cy.contains('ion-button', ' Create Account ').click();
+  it('user enters invalid username and password', () => {
+    cy.get('ion-input[placeholder="Enter Username"]').type('InvalidUsername');
+    cy.get('ion-input[placeholder="Password"]').type('InvalidPassword');
+    cy.contains('ion-button', ' Login ').click();
     cy.wait(3000);
   });
+
+  // it('clicks Signup on the Login page', () => {
+  //   cy.contains('ion-label', "Don't have an account ? Sign Up.").click();
+
+  //   cy.get('ion-input[placeholder="Enter First Name"]').type('TestFirstName');
+  //   cy.get('ion-input[placeholder="Enter Last Name"]').type('TestLastName');
+  //   cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
+  //   cy.get('ion-input[placeholder="Enter Email Address"]').type(
+  //     'testemail@gmail.com'
+  //   );
+  //   cy.get('ion-input[placeholder="Enter Your Password"]').type('TestPassword');
+  //   cy.get('ion-input[placeholder="Confirm Password"]').type('TestPassword');
+
+  //   cy.contains('ion-button', ' Create Account ').click();
+  //   cy.wait(3000);
+  // });
 });
--- cypress/e2e/Register-spec.cy.js
+++ cypress/e2e/Register-spec.cy.js
@@ -1,19 +1,34 @@
 describe('Register Page Test', () => {
-    beforeEach(() => {
-        cy.viewport(375, 667) //iPhone X size
-        cy.visit('http://localhost:4200/register')
-    })
+  beforeEach(() => {
+    cy.viewport(375, 667); //iPhone X size
+    cy.visit('http://localhost:4200/login');
+  });
 
-    it('enter all details needed to register a user', () => {
-        cy.get('ion-input[placeholder="Enter First Name"]').type('TestFirstName');
-        cy.get('ion-input[placeholder="Enter Last Name"]').type('TestLastName');
-        cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
-        cy.get('ion-input[placeholder="Enter Email Address"]').type('testemail@gmail.com');
-        cy.get('ion-input[placeholder="Enter Your Password"]').type('TestPassword');
-        cy.get('ion-input[placeholder="Confirm Password"]').type('TestPassword');
+  it('enter all details needed to register a user', () => {
+    cy.contains('ion-label', "Don't have an account ? Sign Up.").click();
 
-        cy.contains('ion-button', ' Create Account ').click()
-    })
+    cy.get('ion-input[placeholder="Enter First Name"]').type('TestFirstName');
+    cy.get('ion-input[placeholder="Enter Last Name"]').type('TestLastName');
+    cy.get('ion-input[placeholder="Enter Username"]').type('TestUsername');
+    cy.get('ion-input[placeholder="Enter Email Address"]').type(
+      'testemail@gmail.com'
+    );
+    cy.get('ion-input[placeholder="Enter Your Password"]').type('TestPassword');
+    cy.get('ion-input[placeholder="Confirm Password"]').type('TestPassword');
 
-    //cy.visit('http://localhost:4200/preference')
+    //NOTE: since this test has been run the registration will fail as the username already exists
+    cy.contains('ion-button', ' Create Account ').click();
+    cy.wait(3000);
+
+    cy.get('ion-input[placeholder="Enter area here"]').type('Woodstock');
+    cy.get('select[label="Budget"]').select('1 450 000');
+    cy.get('select[label="Bedrooms"]').select('2');
+    cy.get('select[label="Bathrooms"]').select('4');
+    cy.get('select[label="Garage"]').select('2');
+    cy.get('ion-input[label="Extra: "]').type('Pool');
+
+    cy.wait(2000);
+    cy.contains('button', 'Set Preferences').click();
+    cy.wait(3000);
+  });
 });
