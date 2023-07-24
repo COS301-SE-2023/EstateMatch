a62a268dd37eac606d4102481e9116ff04982845
DvZ <danielvzyl1469@gmail.com>
2023-07-24 12:02:59 +0200

    👷 (project) Enviroment setup

--- libs/api/core/feature/src/core.module.ts
+++ libs/api/core/feature/src/core.module.ts
@@ -8,9 +8,12 @@ import { UserModule } from '@estate-match/api/users/feature'
 import { SearchModule } from '@estate-match/api/search/feature';
 import { WebScraperModule } from '@estate-match/api/webscraper/feature';
 
+
+const DATA_BASE_CONNECTION = process.env['DATA_BASE_CONNECTION'] || ''; 
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://teambluecos301:Wtm7JJS8dY0g7vmQ@estate-match.sxjhyyy.mongodb.net/estate-match-db?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(DATA_BASE_CONNECTION),
     PreferenceModule,
     PropertiesModule,
     AuthenticationModule,
