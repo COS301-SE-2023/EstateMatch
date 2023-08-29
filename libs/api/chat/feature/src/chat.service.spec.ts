c3c91d40f461675333bb9e8ad6b7727f942d3d5a
Liam <prinsloo.liam2@gmail.com>
2023-08-29 10:13:47 +0200

    ✨ (feat/chat) updated chat service

--- libs/api/chat/feature/src/chat.service.spec.ts
+++ libs/api/chat/feature/src/chat.service.spec.ts
@@ -3,8 +3,50 @@ import { ChatService } from './chat.service';
 import { CommandBus } from '@nestjs/cqrs';
 import { GetChatCommand, IGetChatRequest, IGetChatResponse, ISetChatRequest, ISetChatResponse, IUpdateChatRequest, IUpdateChatResponse, SetChatCommand, UpdateChatCommand } from '@estate-match/api/chat/util';
 
-describe('UserService', () => {
-   
+describe('ChatService', () => {
+    let service: ChatService;
+    let commandBus: CommandBus;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ChatService,
+                {
+                    provide: CommandBus,
+                    useValue: {
+                        execute: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        service = module.get<ChatService>(ChatService);
+        commandBus = module.get<CommandBus>(CommandBus);
+    });
+
+
+    it('should return a chat', async () => {
+        const request: IGetChatRequest = { 
+            chat: {
+                username: 'testuser'
+            }
+        };
+
+        const commandResponse: IGetChatResponse = { 
+            chat: {
+                id: 'testID',
+                username: 'testuser',
+                message: 'test message'
+            }
+        };
+
+        (commandBus.execute as jest.Mock).mockResolvedValue(commandResponse);
+        const result = await service.getChat(request);
+        expect(commandBus.execute).toHaveBeenCalledWith(
+            expect.any(GetChatCommand),
+        );
+        expect(result).toEqual(commandResponse);
+    });
 });
 
 
