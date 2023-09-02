569dbd217b67d117a17f1f883087d4b63088b77a
DvZ <danielvzyl1469@gmail.com>
2023-09-02 13:05:32 +0200

    🚧 (api) OpenAI chat model added

--- libs/api/chat/feature/src/commands/set-chat.handler.ts
+++ libs/api/chat/feature/src/commands/set-chat.handler.ts
@@ -10,6 +10,9 @@ import { LLMChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HfInference } from '@huggingface/inference';
 import { HuggingFaceInference } from 'langchain/llms/hf';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 const hf = new HfInference(process.env['HF_API_LLM"']);
 
@@ -27,26 +30,47 @@ export class SetChatHandler implements ICommandHandler<SetChatCommand, ISetChatR
         //     inputVariables: ["description"],
         // });
 
-        const chat = new HuggingFaceInference({
-            model: "gpt2",
-            apiKey: process.env['HF_API_LLM'],
-        });
+        // const chat = new HuggingFaceInference({
+        //     model: "google/flan-t5-base",
+        //     apiKey: process.env['HF_API_LLM'],
+        //     maxTokens: 10,
+        // });
 
-        const chatPrompt = new PromptTemplate({
-            template: "Q: {question}",
-            inputVariables: ["question"],
-        });
+        // const chatPrompt = new PromptTemplate({
+        //     template: "Q: {question}",
+        //     inputVariables: ["question"],
+        // });
 
+        // const chainB = new LLMChain({
+        //   prompt: chatPrompt,
+        //   llm: chat,
+
+        // });
+
+        // const resB = await chainB.call({
+        //     question: "Which NFL team won the Super Bowl in the 2010 season?",
+        // });
+        // console.log(resB);
+
+        // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
+        const chat = new ChatOpenAI({ temperature: 0 });
+        const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+        SystemMessagePromptTemplate.fromTemplate(
+            "You are a helpful assistant that translates {input_language} to {output_language}."
+        ),
+        HumanMessagePromptTemplate.fromTemplate("{text}"),
+        ]);
         const chainB = new LLMChain({
-          prompt: chatPrompt,
-          llm: chat,
-
+        prompt: chatPrompt,
+        llm: chat,
         });
 
         const resB = await chainB.call({
-            question: "Which NFL team won the Super Bowl in the 2010 season?",
+        input_language: "English",
+        output_language: "French",
+        text: "I love programming.",
         });
-        console.log(resB);
+        console.log({ resB });
         // const res = await model.call("When is the 2023 Rugby World Cup?");
         // console.log({ res });
     }
