34cf6079d85aa28a0163df25fc5deac52eaae71b
Emilee da Cruz <u21494577@tuks.co.za>
2023-09-07 10:15:04 +0200

    💄 dynamically create messages

--- libs/app/chat/feature/src/chat.page.ts
+++ libs/app/chat/feature/src/chat.page.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
-import { IProperty } from '@estate-match/api/properties/util';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ms-chat-page',
@@ -10,32 +9,47 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage {
-  constructor(private http: HttpClient,
+  constructor(
+    private http: HttpClient,
     private toastController: ToastController,
-    private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute
+  ) {}
 
-    userMessage = '';
-    botMessage  = '';
-    userSentMessage = '';
+  userMessage = '';
+  messages: { text: string; time: string; }[] = [];
 
-    async sendChatMessage() {
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-      const chatUrl = 'api/setChat';
+  async sendChatMessage() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const chatUrl = 'api/setChat';
 
-      
+    const body = {
+      chat: {
+        username: localStorage.getItem('username'),
+        message: this.userMessage,
+      },
+    };
 
-      const body = {
-        chat: {
-          username: localStorage.getItem('username'),
-          message: this.userMessage
-        }
-      }
+    const response = await this.http
+      .post(chatUrl, body, { headers })
+      .toPromise() as { chat: { username: string; message: string } };
 
-      const response = await this.http.post(chatUrl, body, { headers }).toPromise() as {chat: {username: string, message: string}};
-      
-      this.botMessage = response.chat.message;
-      this.userSentMessage = this.userMessage;
+    this.messages.push({
+      text: this.userMessage,
+      time: this.getCurrentTime(),
+    });
 
-    }
+    this.messages.push({
+      text: response.chat.message,
+      time: this.getCurrentTime(),
+    });
+
+    this.userMessage = '';
+  }
+
+  getCurrentTime(): string {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 }
