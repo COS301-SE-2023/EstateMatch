5d20be59c50c43e5d81ff829358896666cadc509
DvZ <danielvzyl1469@gmail.com>
2023-09-11 18:25:22 +0200

    ♻️ (api) Renamed files

--- libs/api/chat/util/src/requests/index.ts
+++ libs/api/chat/util/src/requests/index.ts
@@ -1,3 +1,3 @@
-export * from './get-chat.request';
+export * from './Start-chat.request';
 export * from './set-chat.request';
 export * from './update-chat.request';
--- libs/api/chat/util/src/responses/index.ts
+++ libs/api/chat/util/src/responses/index.ts
@@ -1,3 +1,3 @@
-export * from './get-chat.response'
+export * from './start-chat.response'
 export * from './set-chat.response'
 export * from './update-chat.response'
