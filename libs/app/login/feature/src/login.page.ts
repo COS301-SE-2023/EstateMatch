aaefaa20cd8c6c746766828d6d48f4ce08c20802
DvZ <danielvzyl1469@gmail.com>
2023-09-14 22:02:47 +0200

    🚧 (app) Integration with new database

--- libs/app/login/feature/src/login.page.ts
+++ libs/app/login/feature/src/login.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { IPreference } from '@estate-match/api/prefrences/util';
 
 @Component({
   selector: 'ms-login-page',
@@ -30,6 +31,25 @@ export class LoginPage {
     if(loggedIn.message === 'User Login Success'){
       this.makeToast('Login success');
       sessionStorage.setItem('username', this.username);
+
+      const getPrefURL = 'api/getPreferences';
+
+      const prefBody = {
+        user:{
+          user: this.username,
+        }
+      }
+
+      const userPref = await this.http.post(getPrefURL, prefBody, { headers }).toPromise() as IPreference;
+
+      const remaxRentURL = 'api/RemaxRentScraper';
+
+      const scraperBody = {
+        location: userPref.location,
+      };
+
+      const remaxRent = await this.http.post(remaxRentURL, scraperBody, { headers });
+
       this.router.navigate(['/home'], { replaceUrl: true });
     }else{
       this.makeToast('Login failed');
