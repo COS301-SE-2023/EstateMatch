778d8e3b8e7269518a81bc0926b86d66b62b3537
Liam <prinsloo.liam2@gmail.com>
2023-09-18 16:55:11 +0200

    🚧 (feat/map-poi)added api call

--- libs/api/map/feature/src/commands/get-map.handler.ts
+++ libs/api/map/feature/src/commands/get-map.handler.ts
@@ -1,13 +1,17 @@
 import { CommandHandler } from "@nestjs/cqrs";
 import { ICommandHandler } from "@nestjs/cqrs/dist/interfaces/commands/command-handler.interface";
-import { GetMapCommand } from "@estate-match/api/map/util";
+import { GetMapCommand, IGetMapResponse } from "@estate-match/api/map/util";
+import axios from "axios";
 
 @CommandHandler(GetMapCommand)
 export class GetMapHandler implements ICommandHandler<GetMapCommand> {
 
-    constructor(private readonly mapService: MapService) {}
+    // constructor(private readonly mapService: MapService) {}
 
     async execute(command: GetMapCommand): Promise<IGetMapResponse> {
-        return this.mapService.getMap(command);
+        const apiKey = process.env["GOOGLE_PLACES_API_KEY"] ;
+        console.log(apiKey);
+        const testMap = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7127281%2C-74.0060152&radius=1500&type=school&key=' + apiKey);
+        return testMap.data.results;
     }
 }
