4532d1d68b8b064a2aa3a4fceb9a47b73008c41c
Emilee da Cruz <u21494577@tuks.co.za>
2023-09-18 02:36:36 +0200

    🔥 undoing

--- libs/app/splash-page/feature/src/splash.module.ts
+++ libs/app/splash-page/feature/src/splash.module.ts
@@ -5,13 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { SplashPage } from './splash.page';
 import { SplashRouting } from './splash.routing';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './src/assets/i18n/', '.json');
-}
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -19,14 +13,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     IonicModule,
     SplashRouting, 
     FormsModule, 
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }),
+    HttpClientModule
   ],
   declarations: [SplashPage],
 })
--- libs/app/splash-page/feature/src/splash.page.ts
+++ libs/app/splash-page/feature/src/splash.page.ts
@@ -3,21 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
 import { IProperty } from '@estate-match/api/properties/util';
 import { Router, ActivatedRoute } from '@angular/router';
-import { TranslateService, TranslatePipe } from '@ngx-translate/core';
-
 
 @Component({
   selector: 'ms-splash-page',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
-  providers: [TranslateService]
 })
-export class SplashPage {
-  constructor(
-    private translate: TranslateService,
-  ) {}
-
-  switchLanguage(lang: string) {
-    this.translate.use(lang);
-  }
-}
+export class SplashPage {}
