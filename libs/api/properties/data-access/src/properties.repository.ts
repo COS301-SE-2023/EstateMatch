301e2fa2607db5c524e9375be157d40a1e6358ea
FuryOfD <dharshanng@gmail.com>
2023-09-19 12:16:34 +0200

    ✨ (api) added removePropertyfromUser function

--- libs/api/properties/data-access/src/properties.repository.ts
+++ libs/api/properties/data-access/src/properties.repository.ts
@@ -91,4 +91,32 @@ export class PropertiesRepository {
         }
             return result;
     }
+
+    async removePropertyfromUser(title: string, username: string): Promise<UserModel | null> {
+        const propRemove = await this.propertiesModel.findOneAndUpdate(
+            { title: title },
+            //pulling from user array
+            { $pull: { user: username } }
+          ).exec();
+
+        console.log(propRemove);
+
+        const UserRemove = await this.userModel.findOneAndUpdate(
+            { username: username },
+            //pulling from properties array
+            { $pull: { properties: title } }
+          ).exec();
+        
+        console.log(UserRemove);
+
+        if(!propRemove) {
+            throw new Error('Property not found');  
+        } 
+        else if(!UserRemove) {  
+            throw new Error('User not found');
+        }
+        else {
+            return UserRemove;
+        }
+    }
 }
