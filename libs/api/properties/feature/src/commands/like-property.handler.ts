aae9d81a94a582b537e9a1eb48cad91f166c9e1a
FuryOfD <dharshanng@gmail.com>
2023-09-19 14:33:28 +0200

    🚧 (api) added removePropertyfromUser in execute

--- libs/api/properties/feature/src/commands/like-property.handler.ts
+++ libs/api/properties/feature/src/commands/like-property.handler.ts
@@ -1,11 +1,12 @@
 import { LikePropertyCommand, ILikePropertyResponse } from "@estate-match/api/properties/util";
-import { LikedPropertiesRepository } from "@estate-match/api/properties/data-access";
+import { LikedPropertiesRepository, PropertiesRepository } from "@estate-match/api/properties/data-access";
 import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
 
 @CommandHandler(LikePropertyCommand)
 export class LikePropertyHandler implements ICommandHandler<LikePropertyCommand, ILikePropertyResponse> {
     constructor(
         private readonly repository: LikedPropertiesRepository,
+        private readonly propRepository: PropertiesRepository,
         private readonly publisher: EventPublisher
     ) {}
 
@@ -15,18 +16,35 @@ export class LikePropertyHandler implements ICommandHandler<LikePropertyCommand,
         
         console.log(property);
         //ready to query database
-        const success = await this.repository.setLikedProperty(property);
-
-        if(success){
+       // const success = await this.repository.setLikedProperty(property);
+        //import property repository and call remove property function
+        const username = request.property.user;
+        const title = request.property.title;
+        const remove = await this.propRepository.removePropertyfromUser(title, username);
+       // console.log(remove);
+        if(remove){
             const response = {
-                message: 'property liked'
+                message: 'property removed'
             };
+            console.log(response);
             return response;
         }else{
             const response = {
                 message: 'unexpected error'
             };
+            console.log(response);
             return response;
         }
+        // if(success){
+        //     const response = {
+        //         message: 'property liked'
+        //     };
+        //     return response;
+        // }else{
+        //     const response = {
+        //         message: 'unexpected error'
+        //     };
+        //     return response;
+        // }
     }
 }
