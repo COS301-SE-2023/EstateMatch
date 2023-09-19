d86b52288e5e4ea9f8a8ae4cddc3463413815b7b
Emilee da Cruz <u21494577@tuks.co.za>
2023-09-19 08:55:40 +0200

    ✨ chat translation navbar

--- libs/app/chat/feature/src/chat.page.ts
+++ libs/app/chat/feature/src/chat.page.ts
@@ -2,18 +2,22 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'ms-chat-page',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
+  providers: [TranslateService]
 })
 export class ChatPage {
   constructor(
     private http: HttpClient,
     private toastController: ToastController,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private translate: TranslateService) {
+      this.translate.setDefaultLang('xh');
+    }
 
   userMessage = '';
   messages: { text: string; time: string; userType: 'user' | 'bot' }[] = [];
